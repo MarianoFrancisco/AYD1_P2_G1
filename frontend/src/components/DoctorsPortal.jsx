df522c7b546962eb12a24e8d222498b2b0e8e405
Erick Patzan <erickestuardopatzan@gmail.com>
2024-06-18 00:57:01 -0600

    Se agregó la pantalla principal de pacientes

---
+++ frontend/src/components/DoctorsPortal.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react'
+
+export function DoctorsPortal({ user_id }) {
+  const [doctors, setDoctors] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/medic/patient?user_id=${user_id}`)
+        if (response.ok) {
+          const data = await response.json()
+          setDoctors(data.medics)
+        } else {
+          console.error('Error al obtener datos de médicos:', response.statusText)
+        }
+      } catch (error) {
+        console.error('Error en la solicitud de médicos:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchDoctors()
+  }, [user_id])
+
+  const handleScheduleAppointment = (medicId) => {
+    // Implementa la lógica para programar una cita con el médico específico
+    console.log(`Programando cita con el médico ${medicId}`)
+  }
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen" style={{ height: '90vh' }}>
+        <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"></div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="h-screen flex flex-col items-center" style={{ height: '90vh' }}>
+      {doctors.length > 0 ? (
+        <div className="container mx-auto px-4 py-8">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+            {doctors.map((doctor) => (
+              <div
+                key={doctor.id}
+                className="bg-white rounded-lg overflow-hidden shadow-lg border border-gray-200 hover:shadow-xl"
+                style={{ transition: 'transform 0.3s ease-in-out' }}
+              >
+                <div className="p-4">
+                  <div className="flex items-center justify-center mb-3">
+                    <img
+                      src={`${import.meta.env.VITE_API_URL}/photo/${doctor.photo}`}
+                      alt={`Foto de ${doctor.first_name} ${doctor.last_name}`}
+                      className="rounded-full w-24 h-24 object-cover shadow-md"
+                      style={{
+                        borderRadius: '9999px',
+                        width: '6rem',
+                        height: '6rem',
+                        objectFit: 'cover',
+                        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)'
+                      }}
+                    />
+                  </div>
+                  <div className="text-center mb-3">
+                    <h2 className="text-lg font-semibold mb-1" style={{ fontWeight: '700' }}>{`${doctor.first_name} ${doctor.last_name}`}</h2>
+                    <div className="bg-gray-50 rounded-lg p-3 shadow-inner">
+                      <p className="text-gray-600 text-sm mb-1"><strong>Especialidad:</strong> {doctor.additionalAttribute.specialty.name}</p>
+                      <p className="text-gray-600 text-sm"><strong>Dirección clínica:</strong> {doctor.additionalAttribute.clinicAddress}</p>
+                    </div>
+                  </div>
+                  <div className="flex justify-center mt-3">
+                    <button
+                      onClick={() => handleScheduleAppointment(doctor.id)}
+                      className="bg-gray-700 text-white rounded-md py-2 px-4 text-sm font-medium hover:bg-gray-600"
+                      style={{
+                        borderRadius: '0.375rem',
+                        padding: '0.5rem 1rem',
+                        fontSize: '0.875rem', // Tamaño ligeramente más pequeño que el original
+                        fontWeight: '500',
+                        transition: 'background-color 0.3s ease-in-out, transform 0.3s ease-in-out',
+                        margin: '0.1rem', // Ajuste mínimo al margen
+                      }}
+                    >
+                      Programar cita
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-grow items-center justify-center" style={{ height: '90vh' }}>
+          <div className="text-center">
+            <p className="text-gray-600 text-lg mb-4">No hay médicos disponibles para mostrar.</p>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
--- frontend/src/pages/PatientHome.jsx
+++ frontend/src/pages/PatientHome.jsx
@@ -1,11 +1,15 @@
-import { useEffect } from 'react'
-import { decodeToken } from 'react-jwt'
+import React, { useState, useEffect, useRef } from 'react'
 import Cookies from 'js-cookie'
+import { decodeToken } from 'react-jwt'
+import { DoctorsPortal } from '../components/DoctorsPortal'
 
 export function PatientHome({ onLogout }) {
-  useEffect(() => {
-    document.title = 'Patient Home | MediCare'
-  })
+  const [selectedOption, setSelectedOption] = useState('Portal de médicos')
+  const [menuOpen, setMenuOpen] = useState(false)
+  const menuRef = useRef(null)
+  const [hoverMediCare, setHoverMediCare] = useState(false)
+  const [hoverMenu, setHoverMenu] = useState(false)
+  const [user, setUser] = useState(null)
 
   const getUserData = () => {
     const token = Cookies.get('token')
@@ -15,14 +19,108 @@ export function PatientHome({ onLogout }) {
       return decodedUser
     } else {
       console.log('No se encontró ningún token en las cookies.')
+      return null
     }
   }
 
+  useEffect(() => {
+    const userData = getUserData()
+    if (userData) {
+      setUser(userData)
+    }
+  }, []) // Agrega [] para ejecutar solo una vez al montar el componente
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option)
+    setMenuOpen(false)
+    if (option === 'Cerrar sesión') {
+      onLogout()
+    }
+  }
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  const handleMediCareHover = () => {
+    if (!menuOpen) {
+      setHoverMediCare(true)
+    }
+  }
+
+  const handleMediCareLeave = () => {
+    setHoverMediCare(false)
+  }
+
+  const handleMenuHover = () => {
+    if (!menuOpen) {
+      setHoverMenu(true)
+    }
+  }
+
+  const handleMenuLeave = () => {
+    setHoverMenu(false)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuOpen(false)
+      }
+    }
+
+    document.title = 'Patient Home | MediCare';
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
   return (
-    <>
-      <h1>PatientHome</h1>
-      <p>{JSON.stringify(getUserData())}</p>
-      <button type='button' onClick={onLogout}>Cerrar sesión</button>
-    </>
+    <div className="min-h-screen bg-white text-black">
+      <nav className="bg-gray-900 py-4 px-24 flex justify-between items-center text-white">
+        <div
+          className={`flex items-center space-x-4 ${hoverMediCare ? 'text-gray-400' : 'text-white'}`}
+          onMouseEnter={handleMediCareHover}
+          onMouseLeave={handleMediCareLeave}
+        >
+          <div className={`text-xl font-bold mr-8 text-white`}>MediCare</div>
+          <button
+            onClick={() => handleOptionClick('Portal de médicos')}
+            className={`transition-colors ${selectedOption === 'Portal de médicos' ? 'text-white' : 'text-gray-400'}`}
+          >
+            Portal de médicos
+          </button>
+          <button
+            onClick={() => handleOptionClick('Citas activas')}
+            className={`transition-colors ${selectedOption === 'Citas activas' ? 'text-white' : 'text-gray-400'}`}
+          >
+            Citas activas
+          </button>
+        </div>
+        <div className="relative" ref={menuRef} onMouseEnter={handleMenuHover} onMouseLeave={handleMenuLeave}>
+          <button onClick={toggleMenu}>
+            <span className={menuOpen || hoverMenu ? 'text-gray-400' : 'text-white'}>
+              {user ? `${user.first_name} ${user.last_name}` : 'Nombre de usuario'} &#x25BE;
+            </span>
+          </button>
+          {menuOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-gray-800 border border-gray-700 rounded-md shadow-lg" style={{ zIndex: 9999 }}>
+              <button
+                onClick={() => handleOptionClick('Cerrar sesión')}
+                className="block px-4 py-2 hover:bg-gray-700 w-full text-left text-white"
+              >
+                Cerrar sesión
+              </button>
+            </div>
+          )}
+        </div>
+      </nav>
+      <main className="p-0">
+        {selectedOption === 'Portal de médicos' && <DoctorsPortal user_id={user ? user.id : null} />}
+        {selectedOption === 'Citas activas' && <p>Citas activas</p>}
+      </main>
+    </div>
   )
 }
