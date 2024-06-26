9c1fff78ab1f673f7439cb39b3dc1072706fa768
Erick Patzan <erickestuardopatzan@gmail.com>
2024-06-26 00:04:55 -0600

    Correción del historial de citas y mensaje de confirmación

--- frontend/src/components/ActiveAppointments.jsx
+++ frontend/src/components/ActiveAppointments.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Swal from 'sweetalert2'
 
 export function ActiveAppointments({ userId }) {
@@ -28,28 +28,41 @@ export function ActiveAppointments({ userId }) {
   }, [userId])
 
   const cancelAppointment = async (appointmentId) => {
-    try {
-      console.log(appointmentId)
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/appointment/patient/cancelled/${appointmentId}`, {
-        method: 'PATCH'
-      })
-      if (!response.ok) {
-        throw new Error('Failed to cancel appointment')
-      }
+    // Confirmación usando SweetAlert2
+    const result = await Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'Esta acción no se puede revertir. ¿Quieres cancelar esta cita?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí',
+      cancelButtonText: 'No',
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33'
+    })
 
-      setAppointments(appointments.filter(appointment => appointment.id !== appointmentId))
-      Swal.fire({
-        icon: 'success',
-        title: 'Cita cancelada',
-        text: 'La cita ha sido cancelada exitosamente.'
-      })
-    } catch (error) {
-      console.error('Error cancelling appointment:', error)
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Hubo un problema al cancelar la cita. Por favor, inténtelo nuevamente.'
-      })
+    if (result.isConfirmed) {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/appointment/patient/cancelled/${appointmentId}`, {
+          method: 'PATCH'
+        })
+        if (!response.ok) {
+          throw new Error('Failed to cancel appointment')
+        }
+
+        setAppointments(appointments.filter(appointment => appointment.id !== appointmentId))
+        Swal.fire({
+          icon: 'success',
+          title: 'Cita cancelada',
+          text: 'La cita ha sido cancelada exitosamente.'
+        })
+      } catch (error) {
+        console.error('Error cancelling appointment:', error)
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Hubo un problema al cancelar la cita. Por favor, inténtelo nuevamente.'
+        })
+      }
     }
   }
 
@@ -73,32 +86,42 @@ export function ActiveAppointments({ userId }) {
     <div className="container mx-auto mt-4">
       <div className="overflow-x-auto">
         <table className="min-w-full bg-white border border-gray-300 shadow-lg rounded-lg overflow-hidden">
-          <thead className="bg-gray-900 text-white uppercase">
+          <thead className="bg-gray-800 text-white">
             <tr>
-              <th className="py-3 px-4 text-left">Fecha</th>
-              <th className="py-3 px-4 text-left">Hora</th>
-              <th className="py-3 px-4 text-left">Médico</th>
-              <th className="py-3 px-4 text-left">Clínica</th>
-              <th className="py-3 px-4 text-left">Motivo</th>
-              <th className="py-3 px-4 text-left">Acciones</th>
+              <th className="w-1/12 py-2">Fecha</th>
+              <th className="w-2/12 py-2">Hora</th>
+              <th className="w-3/12 py-2">Médico</th>
+              <th className="w-3/12 py-2">Clínica</th>
+              <th className="w-3/12 py-2">Motivo</th>
+              <th className="w-1/12 py-2">Acciones</th>
             </tr>
           </thead>
           <tbody>
-            {appointments.map((appointment, index) => (
-              <tr
-                key={appointment.id}
-                className={`${index % 2 === 0 ? 'bg-gray-50' : 'bg-gray-100'} transition-colors`}
-              >
-                <td className="py-3 px-4 whitespace-nowrap">{appointment.date}</td>
-                <td className="py-3 px-4">{`${appointment.available_time_slot.start_time} - ${appointment.available_time_slot.end_time}`}</td>
-                <td className="py-3 px-4">{`${appointment.medic?.first_name} ${appointment.medic?.last_name}`}</td>
-                <td className="py-3 px-4">{appointment.medic?.additionalAttribute.clinicAddress}</td>
-                <td className="py-3 px-4">{appointment.reason}</td>
-                <td className="py-3 px-4">
+            {appointments.map((appointment) => (
+              <tr key={appointment.id} className="bg-white border-b border-gray-200">
+                <td className="py-2 text-center">{appointment.date}</td>
+                <td className="py-2 text-center">{`${appointment.available_time_slot.start_time} - ${appointment.available_time_slot.end_time}`}</td>
+                <td className="py-2">
+                  <div className="flex items-center">
+                    <img
+                      src={`${import.meta.env.VITE_API_URL}/photo/${appointment.medic.photo}`}
+                      alt={`${appointment.medic.first_name} ${appointment.medic.last_name}`}
+                      className="w-10 h-10 rounded-full mr-4"
+                    />
+                    <div>
+                      <p>{`${appointment.medic.first_name} ${appointment.medic.last_name}`}</p>
+                      <p className="text-sm text-gray-600">{appointment.medic.email}</p>
+                      <p className="text-sm text-gray-600">{appointment.medic.additionalAttribute.specialty.name}</p>
+                    </div>
+                  </div>
+                </td>
+                <td className="py-2">{appointment.medic.additionalAttribute.clinicAddress}</td>
+                <td className="py-2">{appointment.reason}</td>
+                <td className="py-2">
                   <button
                     onClick={() => cancelAppointment(appointment.id)}
                     className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700 transition-colors"
-                    style={{fontWeight: 'bold'}}
+                    style={{ fontWeight: 'bold', marginRight: '10px' }}
                   >
                     Cancelar
                   </button>
