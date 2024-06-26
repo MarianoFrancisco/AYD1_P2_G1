a28385abfaaf4f28799080c4a209171539cb42a6
María Cecilia Cotzajay López <3033707770108@ingenieria.usac.edu.gt>
2024-06-25 22:26:18 -0600

    E2E Login y actualización de perfil completado

--- frontend/cypress/e2e/login_paciente.cy.js
+++ frontend/cypress/e2e/login_paciente.cy.js
@@ -9,16 +9,33 @@ describe('Login-Paciente Test', () => {
       cy.wait(1000);
       cy.contains('MediCare').should('be.visible');
       cy.wait(1000);
-      cy.get(':nth-child(3) > .flex').click(); //cy.get('form').submit();
+      cy.get(':nth-child(3) > .flex').click();
       cy.wait(1000);
       // Ingresa a página principal 
       cy.url().should('include', '/patient-home');
-      cy.wait(1000);
+      cy.wait(1500);
       // Editar Perfil
-      //cy.contains();
-      //cy.wait(1000);
-      // Cierra sesión y regresa a Login
       cy.get('button > .text-white').click();
+      cy.wait(1500);
+      cy.get('.absolute > :nth-child(1)').click(); //Boton editar Perfil
+      cy.wait(1000);
+      cy.get('#first_name').clear().type("Angel Esau");
+      cy.wait(1000);
+      cy.get('#last_name').clear().type("Cardenas");
+      cy.wait(1000);
+      cy.get('#birth_date').invoke('val', '1994-03-10').trigger('change');
+      cy.wait(1500);
+      cy.get('.bg-gray-800').click(); //Boton Guardar cambios
+      cy.wait(3000);
+      cy.get('.swal2-confirm').click();
+      cy.wait(1500);
+      //Regresa a página principal
+      cy.get('.bg-gray-900 > .flex > :nth-child(2)').click();
+      cy.wait(3000);
+      cy.contains('Angel Esau').should('be.visible');;
+      cy.wait(1000);
+      // Cierra sesión y regresa a Login
+      cy.get('.relative > :nth-child(1) > .text-gray-400').click();
       cy.wait(1500);
       cy.contains('Cerrar sesión').click();
       cy.wait(1000);
