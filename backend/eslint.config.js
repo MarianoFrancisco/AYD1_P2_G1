205e6588a42a951bab90ab5ca3411cd88cb18cca
María Cecilia Cotzajay López <3033707770108@ingenieria.usac.edu.gt>
2024-06-27 14:49:06 -0600

    ci_g1

---
+++ backend/eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+      node: true,
+      es6: true,
+    },
+    extends: ['airbnb-base'],
+    parserOptions: {
+      ecmaVersion: 2021,
+    },
+    rules: {
+      // Aquí puedes agregar reglas específicas según tus necesidades
+    },
+  };
+  
