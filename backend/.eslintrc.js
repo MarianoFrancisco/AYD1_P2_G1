6d5bf265dcb32c9cfbe89fac4299c8bed0217ce0
María Cecilia Cotzajay López <3033707770108@ingenieria.usac.edu.gt>
2024-06-27 14:41:50 -0600

    ci_g1

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,11 +1,31 @@
 module.exports = {
-    extends: [
-      'eslint:recommended',
-      'plugin:prettier/recommended',
-    ],
-    plugins: ['prettier'],
-    rules: {
-      'prettier/prettier': 'error',
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-  };
-  
+  },
+  rules: {
+    // Aquí puedes configurar reglas adicionales o sobrescribir las predeterminadas
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
